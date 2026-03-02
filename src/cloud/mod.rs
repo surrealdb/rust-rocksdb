be56a0f7bb9d417e4b52e33b6757193f1831ebcf
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2026-03-02 13:24:11 +0000

    Add bindings for `CloudOptimisticTransactionDB`
    Add Rust wrapper for the RocksDB custom `CloudOptimisticTransactionDB`, which provides optimistic transactions over a cloud-backed database.

--- src/cloud/mod.rs
+++ src/cloud/mod.rs
@@ -4,6 +4,7 @@ mod cloud_credentials;
 mod cloud_db;
 mod cloud_fs;
 mod cloud_fs_options;
+mod cloud_optimistic_transaction_db;
 
 pub use cloud_bucket_options::CloudBucketOptions;
 pub use cloud_checkpoint_options::CloudCheckpointOptions;
@@ -11,3 +12,4 @@ pub use cloud_credentials::{AwsAccessType, CloudCredentials};
 pub use cloud_db::CloudDB;
 pub use cloud_fs::CloudFileSystem;
 pub use cloud_fs_options::CloudFileSystemOptions;
+pub use cloud_optimistic_transaction_db::CloudOptimisticTransactionDB;
